74df7b02ea1b4961b14ce0a91f5d92c0a4a034e2
vansar <vansar.w@gmail.com>
2014-01-04 17:00:15 +0800

    Evaluate_Reverse_Polish_Notation.cpp is Committed for the first time

--- Evaluate_Reverse_Polish_Notation/Evaluate_Reverse_Polish_Notation.cpp
+++ Evaluate_Reverse_Polish_Notation/Evaluate_Reverse_Polish_Notation.cpp
@@ -18,16 +18,59 @@
 
 #include<iostream>
 #include<vector>
+#include<string.h>
 using namespace std;
 class Solution{
 public:
+	int isOperater(string *s){
+		return (*s == "+" | *s == "-" | *s == "*" | *s == "/");
+	}
 	int evalRPN(vector<string> &tokens){
-
+		vector<int> stack(1000);
+		int index = 0, operand_left = 0, operand_right = 0;
+		for(index = 0; index < tokens.size(); index++){
+			if(!isOperater(&tokens[index])){
+				stack.push_back(atoi(tokens[index].c_str()));
+			}else{
+				if(tokens[index] == "+"){
+					operand_left  = stack.back();
+					stack.pop_back();
+					operand_right = stack.back();
+					stack.pop_back();
+					stack.push_back(operand_right + operand_left);
+				}else if(tokens[index] ==  "-"){
+					operand_left  = stack.back();
+					stack.pop_back();
+					operand_right = stack.back();
+					stack.pop_back();
+					stack.push_back(operand_right - operand_left);
+				}else if(tokens[index] ==  "*"){
+					operand_left  = stack.back();
+					stack.pop_back();
+					operand_right = stack.back();
+					stack.pop_back();
+					stack.push_back(operand_right * operand_left);
+				}else if(tokens[index] ==  "/"){
+					operand_left  = stack.back();
+					stack.pop_back();
+					operand_right = stack.back();
+					stack.pop_back();
+					stack.push_back(operand_right / operand_left);
+				}
+			}
+		}
+		return stack.back();
 	}
 };
 
 //unit testing
 int main(){
 	Solution s;
+	const string str1[] = {"2", "1", "+", "3", "*"};
+	vector<string> svec(str1, str1 + 5);
+	cout<<s.evalRPN(svec)<<endl;
+	const string str2[] = {"4", "13", "5", "/", "+"};
+	svec.assign(str2, str2 + 5);
+	cout<<s.evalRPN(svec)<<endl;
 	return 0;
 }
