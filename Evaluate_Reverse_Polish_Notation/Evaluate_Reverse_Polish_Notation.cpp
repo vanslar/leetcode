d62674c13d635af9802043c1dc41f03e6d73b713
vansar <vansar.w@gmail.com>
2014-01-04 15:36:21 +0800

    Problem 1: Evaluate_Reverse_Polish_Notation

---
+++ Evaluate_Reverse_Polish_Notation/Evaluate_Reverse_Polish_Notation.cpp
@@ -0,0 +1,33 @@
+/*Problem:
+* Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+* 
+* Valid operators are +, -, *, /. Each operand may be an integer or another expression.
+* 
+* Some examples:
+*   ["2", "1", "+", "3", "*"] -> ((2 + 1) * 3) -> 9
+*   ["4", "13", "5", "/", "+"] -> (4 + (13 / 5)) -> 6
+* 
+* 
+* class Solution {
+* public:
+*     int evalRPN(vector<string> &tokens) {
+* 	        
+*     }
+* };
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution{
+public:
+	int evalRPN(vector<string> &tokens){
+
+	}
+};
+
+//unit testing
+int main(){
+	Solution s;
+	return 0;
+}
